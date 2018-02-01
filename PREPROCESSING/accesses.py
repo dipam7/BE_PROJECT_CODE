96154ec762badb444e7184c49a4da2b7a2fdd27c
Dipam Vasani <dipam44@gmail.com>
2018-02-01 10:00:01 +0530

    Add more entries in access table

--- PREPROCESSING/accesses.py
+++ PREPROCESSING/accesses.py
@@ -18,7 +18,7 @@ with open('generated_access_data.csv', 'w') as csv_write_file:
     # they will access data relevant to their specialization
     # there will be no emergency cases
     # and access will always be granted
-    for i in range(5):
+    for i in range(10):
         for key in patients_doctor:
 
             doctor = patients_doctor[key]
@@ -45,46 +45,47 @@ with open('generated_access_data.csv', 'w') as csv_write_file:
     # result is initially set to yes for the doctors with good prev history
     result = 'yes'
     emergency = 'no'
-    for key in patients_doctor:
+    for i in range(2):
+        for key in patients_doctor:
 
-        doctor = patients_doctor[key]
+            doctor = patients_doctor[key]
 
-        # 9 am to 9 pm
-        hours = random.randint(9, 20)
-        # random minutes
-        minutes = random.randint(0, 59)
+            # 9 am to 9 pm
+            hours = random.randint(9, 20)
+            # random minutes
+            minutes = random.randint(0, 59)
 
-        # zfill is used because if minutes is in single digits
-        # a zero must be appended before it
-        time = str(hours).zfill(2) + ":" + str(minutes).zfill(2)
+            # zfill is used because if minutes is in single digits
+            # a zero must be appended before it
+            time = str(hours).zfill(2) + ":" + str(minutes).zfill(2)
 
-        accesses = int(doctors_grants[doctor][0])
-        grants = int(doctors_grants[doctor][1])
-        # print(accesses)
-        # print(grants)
+            accesses = int(doctors_grants[doctor][0])
+            grants = int(doctors_grants[doctor][1])
+            # print(accesses)
+            # print(grants)
 
-        # if the grants are less than 75% then choose on random whether to
-        # give access or no
-        if grants < int(0.75 * accesses):
-            result = random.choice(['yes', 'no'])
-            if result == 'yes':
-                emergency = 'yes'
-            else:
-                emergency = 'no'
+            # if the grants are less than 75% then choose on random whether to
+            # give access or no
+            if grants < int(0.75 * accesses):
+                result = random.choice(['yes', 'no'])
+                if result == 'yes':
+                    emergency = 'yes'
+                else:
+                    emergency = 'no'
 
-        # but if the grants are less than 30% of accesses
-        # give less accesses
-        if grants < int(0.3 * accesses):
-            # probability of no is greater
-            result = random.choice(['yes', 'no', 'no'])
-            if result == 'yes':
-                emergency = 'yes'
-            else:
-                emergency = 'no'
+            # but if the grants are less than 30% of accesses
+            # give less accesses
+            if grants < int(0.3 * accesses):
+                # probability of no is greater
+                result = random.choice(['yes', 'no', 'no'])
+                if result == 'yes':
+                    emergency = 'yes'
+                else:
+                    emergency = 'no'
 
-        random_pc = random.choice(list(doctors_pc.keys()))
+            random_pc = random.choice(list(doctors_pc.keys()))
 
-        csv_writer.writerow({'p_id': key, 'd_id': doctor, 'location_of_access': doctors_pc[random_pc], 'time_of_access': time, 'data_requested': 'relevant', 'emergency': emergency, 'access_granted': result})
+            csv_writer.writerow({'p_id': key, 'd_id': doctor, 'location_of_access': doctors_pc[random_pc], 'time_of_access': time, 'data_requested': 'relevant', 'emergency': emergency, 'access_granted': result})
 
     # average case 2:
     # same as average case 1 but instead of wrong computer
@@ -219,44 +220,45 @@ with open('generated_access_data.csv', 'w') as csv_write_file:
     # 90 - 50% based on emergency
     result = 'yes'
     emergency = 'no'
-    for key in patients_doctor:
+    for i in range(2):
+        for key in patients_doctor:
 
-        doctor = patients_doctor[key]
+            doctor = patients_doctor[key]
 
-        # 9 am to 9 pm
-        hours = random.randint(9, 20)
-        # random minutes
-        minutes = random.randint(0, 59)
+            # 9 am to 9 pm
+            hours = random.randint(9, 20)
+            # random minutes
+            minutes = random.randint(0, 59)
 
-        accesses = int(doctors_grants[doctor][0])
-        grants = int(doctors_grants[doctor][1])
-        # print(accesses)
-        # print(grants)
+            accesses = int(doctors_grants[doctor][0])
+            grants = int(doctors_grants[doctor][1])
+            # print(accesses)
+            # print(grants)
 
-        # if the grants are less than 90% then choose on random whether to
-        # give access or no
-        if grants < int(0.9 * accesses):
-            result = random.choice(['yes', 'no'])
-            if result == 'yes':
-                emergency = 'yes'
-            else:
-                emergency = 'no'
+            # if the grants are less than 90% then choose on random whether to
+            # give access or no
+            if grants < int(0.9 * accesses):
+                result = random.choice(['yes', 'no'])
+                if result == 'yes':
+                    emergency = 'yes'
+                else:
+                    emergency = 'no'
 
-        # but if the grants are less than 30% of accesses
-        # give less number of accesses
-        if grants < int(0.3 * accesses):
-            # probability of no is greater
-            result = random.choice(['yes', 'no', 'no'])
-            if result == 'yes':
-                emergency = 'yes'
-            else:
-                emergency = 'no'
+            # but if the grants are less than 30% of accesses
+            # give less number of accesses
+            if grants < int(0.3 * accesses):
+                # probability of no is greater
+                result = random.choice(['yes', 'no', 'no'])
+                if result == 'yes':
+                    emergency = 'yes'
+                else:
+                    emergency = 'no'
 
-        # zfill is used because if minutes is in single digits
-        # a zero must be appended before it
-        time = str(hours).zfill(2) + ":" + str(minutes).zfill(2)
+            # zfill is used because if minutes is in single digits
+            # a zero must be appended before it
+            time = str(hours).zfill(2) + ":" + str(minutes).zfill(2)
 
-        csv_writer.writerow({'p_id': key, 'd_id': doctor, 'location_of_access': doctors_pc[doctor], 'time_of_access': time, 'data_requested': 'slightly-irrelevant', 'emergency': emergency, 'access_granted': result})
+            csv_writer.writerow({'p_id': key, 'd_id': doctor, 'location_of_access': doctors_pc[doctor], 'time_of_access': time, 'data_requested': 'slightly-irrelevant', 'emergency': emergency, 'access_granted': result})
 
     # average case 5:
     # doctors will access data from the wrong pc
@@ -382,44 +384,45 @@ with open('generated_access_data.csv', 'w') as csv_write_file:
     # less than 50 no access
     result = 'yes'
     emergency = 'no'
-    for key in patients_doctor:
+    for i in range(2):
+        for key in patients_doctor:
 
-        doctor = patients_doctor[key]
+            doctor = patients_doctor[key]
 
-        # 9 am to 9 pm
-        hours = random.randint(9, 20)
-        # random minutes
-        minutes = random.randint(0, 59)
+            # 9 am to 9 pm
+            hours = random.randint(9, 20)
+            # random minutes
+            minutes = random.randint(0, 59)
 
-        # zfill is used because if minutes is in single digits
-        # a zero must be appended before it
-        time = str(hours).zfill(2) + ":" + str(minutes).zfill(2)
+            # zfill is used because if minutes is in single digits
+            # a zero must be appended before it
+            time = str(hours).zfill(2) + ":" + str(minutes).zfill(2)
 
-        accesses = int(doctors_grants[doctor][0])
-        grants = int(doctors_grants[doctor][1])
-        # print(accesses)
-        # print(grants)
+            accesses = int(doctors_grants[doctor][0])
+            grants = int(doctors_grants[doctor][1])
+            # print(accesses)
+            # print(grants)
 
-        # if the grants are less than 95% then choose on random whether to
-        # give access or no
-        if grants < int(0.95 * accesses):
-            result = random.choice(['yes', 'no', 'no'])
-            if result == 'yes':
-                emergency = 'yes'
-            else:
-                emergency = 'no'
+            # if the grants are less than 95% then choose on random whether to
+            # give access or no
+            if grants < int(0.95 * accesses):
+                result = random.choice(['yes', 'no', 'no'])
+                if result == 'yes':
+                    emergency = 'yes'
+                else:
+                    emergency = 'no'
 
-        # but if the grants are less than 30% of accesses
-        # never give access
-        if grants < int(0.4 * accesses):
-            # probability of no is greater
-            result = random.choice(['yes', 'no', 'no', 'no'])
-            if result == 'yes':
-                emergency = 'yes'
-            else:
-                emergency = 'no'
+            # but if the grants are less than 30% of accesses
+            # never give access
+            if grants < int(0.4 * accesses):
+                # probability of no is greater
+                result = random.choice(['yes', 'no', 'no', 'no'])
+                if result == 'yes':
+                    emergency = 'yes'
+                else:
+                    emergency = 'no'
 
-        csv_writer.writerow({'p_id': key, 'd_id': doctor, 'location_of_access': doctors_pc[doctor], 'time_of_access': time, 'data_requested': 'irrelevant', 'emergency': emergency, 'access_granted': result})
+            csv_writer.writerow({'p_id': key, 'd_id': doctor, 'location_of_access': doctors_pc[doctor], 'time_of_access': time, 'data_requested': 'irrelevant', 'emergency': emergency, 'access_granted': result})
 
     # average case 8:
     # wrong pc and highly irrelevant data
